707f00c20e87ae3ce71fa43edd96e10823a80f1d
Anthony <anthonnhani@gmail.com>
2026-01-21 11:28:35 -0700

    Implemented basic static elements of the HTML template

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import {Task} from "./backend/task.js";
 import { ToDo } from "./backend/todo.js";
 import { Project } from "./backend/project.js";
 import { compareAsc, format } from "date-fns";
+import "./styles/main-page.css";
 
 
